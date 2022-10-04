f653559fada6d0b935ce7cf3e9a6e9493b6d4997
seeys <es2008e@naver.com>
2022-10-05 00:08:17 +0900

    update delect strict mode

--- src/api/CallbackGoogle.jsx
+++ src/api/CallbackGoogle.jsx
@@ -3,24 +3,16 @@ import React from "react";
 import { useState } from "react";
 import { useEffect } from "react";
 
-axios.defaults.withCredentials = true;
 const CallbackGoogle = () => {
-  const code = new URL(window.location.href).searchParams.get("code");
-  console.log(code);
-  const getToken = (c) => {
-    axios.get(`https://ezoz-trip.com/auth/google?code=${c}`).then((res) => {
-      console.log(res);
-      setIsGet(res);
-    });
-  };
+  useEffect(() => {
+    const code = new URL(window.location.href).searchParams.get("code");
+    console.log("hi");
+    // axios.get(`https://ezoz-trip.com/auth/google?code=${code}`).then((res) => {
+    //   console.log(res);
+    // });
+  }, []);
 
-  const [isGet, setIsGet] = useState("");
-
-  if (code) {
-    getToken(code);
-  }
-
-  return <div>{isGet ? <p>get login</p> : <p>not login</p>}</div>;
+  return <div>hi</div>;
 };
 
 export default CallbackGoogle;
