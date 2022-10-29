f79d0484d1dee3d3771b4d54c4d8754f664909c8
seeys <es2008e@naver.com>
2022-10-29 23:15:58 +0900

    add apply button components

--- src/components/Home/Apply/ApplyButton.jsx
+++ src/components/Home/Apply/ApplyButton.jsx
@@ -1,7 +1,19 @@
 import React from "react";
+import styled from "styled-components";
 
-const ApplyButton = () => {
-  return <div>ApplyButton</div>;
+const Button = styled.div`
+  background-color: #424242;
+  color: white;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  margin: 0 auto;
+  width: 800px;
+  font-size: 1.4rem;
+`;
+
+const ApplyButton = (props) => {
+  return <Button>{props}</Button>;
 };
 
 export default ApplyButton;
