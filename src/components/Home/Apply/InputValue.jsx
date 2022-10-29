1cd9d31d092bafa76c45c5131f60dbf2b3ce0edf
seeys <es2008e@naver.com>
2022-10-29 23:13:27 +0900

    add apply inputvalue components

--- src/components/Home/Apply/InputValue.jsx
+++ src/components/Home/Apply/InputValue.jsx
@@ -1,7 +1,18 @@
 import React from "react";
+import styled from "styled-components";
 
-const InputValue = () => {
-  return <div>InputValue</div>;
+const Input = styled.input`
+  width: 300px;
+  border: 1px solid black;
+  text-align: center;
+  font-size: 1.2rem;
+  ::placeholder {
+    color: rgba(0, 0, 0, 0.4);
+  }
+`;
+
+const InputValue = ({ ...rest }) => {
+  return <Input {...rest} />;
 };
 
 export default InputValue;
